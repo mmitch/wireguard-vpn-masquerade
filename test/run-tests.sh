2a3ac113f510f2b4b0d29c9bfbcfc95d93dddfd0
Christian Garbs <mitch@cgarbs.de>
2019-07-28 21:29:50 +0200

    fix run-tests.sh --watch

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -5,7 +5,7 @@ set -e
 # run in autowatch mode with "--watch"
 if [ "$1" = "--watch" ]; then
     ./run-tests.sh || true
-    while inotifywait -e modify -e move -e move_self -e create -e delete -e delete_self tests tests/* test-functions.sh run-tests.sh; do
+    while inotifywait -e modify -e move -e move_self -e create -e delete -e delete_self tests tests/* run-tests.sh wg-conf.* ../wg-conf; do
         ./run-tests.sh || true
     done
     exit 0
